cc0ced6175c23ec1758507d172d6be3d5ba9855c
Moritz Eckert <MoritzEckert@web.de>
2023-06-15 23:55:35 +0200

    Fixed Mistake

--- ToMaChess.py
+++ ToMaChess.py
@@ -2,12 +2,12 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 from scripts.window_main import Window_Main
 from scripts.functions_util import get_image, remove_temporary_files
-from scripts.class_database_handler import database_handler
+from scripts.class_database_handler import DATABASE_HANDLER
 
 
 if __name__ == "__main__":
     remove_temporary_files()
-    database_handler.create_tables()
+    DATABASE_HANDLER.create_tables()
     app = QApplication([])
     app.setWindowIcon(QIcon(get_image("logo.png")))
     main_window = Window_Main()
